3c731e4b793bba196ac211c0cf13e45d8b4a08f2
r4nx <mod34@yandex.ru>
2022-01-10 11:06:16 +0200

    Add more hooks tests

--- tests/hooks_tests.cpp
+++ tests/hooks_tests.cpp
@@ -1,7 +1,12 @@
 #include <cyanide/memory/hook_backend_subhook.hpp>
 #include <cyanide/memory/hook_frontend.hpp>
+#include <cyanide/memory/signature_scanner_win.hpp>
+#include <cyanide/types/safe_pun.hpp>
 
 #include <catch2/catch_test_macros.hpp>
+
+#include <functional> // std::bind_front, std::function
+#include <utility>    // std::move
 
 __declspec(noinline) int test_func_a(int x, int y)
 {
@@ -14,7 +19,21 @@ __declspec(noinline) int test_func_a(int x, int y)
     return y / x;
 }
 
-TEST_CASE("Unhooked function")
+// Class for testing member function callback, used below
+class Hooker {
+public:
+    Hooker(int replacing_value) : replacing_value_{replacing_value} {}
+
+    int callback(int x, int y)
+    {
+        return replacing_value_;
+    }
+
+private:
+    int replacing_value_ = 0;
+};
+
+TEST_CASE("Unhooked function", "[memory][hooks]")
 {
     constexpr int x               = 3;
     constexpr int y               = 4;
@@ -25,23 +44,58 @@ TEST_CASE("Unhooked function")
     REQUIRE(actual_result == expected_result);
 }
 
-TEST_CASE("Detour")
+TEST_CASE("Detour with lambda callback", "[memory][hooks]")
 {
-    cyanide::memory::SubhookDetourBackend backend;
-    cyanide::memory::DetourFrontend       frontend{
-        &backend,
-        &test_func_a,
-        [](decltype(&test_func_a) orig, int x, int y) -> int {
-            return orig(x, y) + 5;
-        }};
+    constexpr int x                      = 3;
+    constexpr int y                      = 4;
+    constexpr int expected_result        = 2;
+    constexpr int expected_result_hooked = 7;
 
-    frontend.install();
+    {
+        cyanide::memory::SubhookDetourBackend backend;
+        cyanide::memory::DetourFrontend       frontend{
+            &backend,
+            &test_func_a,
+            [](decltype(&test_func_a) orig, int x, int y) -> int {
+                return orig(x, y) + 5;
+            }};
 
-    constexpr int x               = 3;
-    constexpr int y               = 4;
-    constexpr int expected_result = 7;
+        frontend.install();
+
+        const int actual_result = test_func_a(x, y);
+        REQUIRE(actual_result == expected_result_hooked);
+    }
+
+    // Scope exited, the function must be unhooked at this point
+    const int actual_result = test_func_a(x, y);
+    REQUIRE(actual_result == expected_result);
+}
+
+TEST_CASE("Detour with member function callback", "[memory][hooks]")
+{
+    constexpr int x                      = 3;
+    constexpr int y                      = 4;
+    constexpr int expected_result        = 2;
+    constexpr int expected_result_hooked = 5;
+
+    {
+        Hooker hooker{5};
+
+        std::function<decltype(test_func_a)> callback{
+            std::bind_front(&Hooker::callback, &hooker)};
+
+        cyanide::memory::SubhookDetourBackend backend;
+        cyanide::memory::DetourFrontend       frontend{
+            &backend,
+            &test_func_a,
+            std::move(callback)};
+
+        frontend.install();
+
+        const int actual_result = test_func_a(x, y);
+        REQUIRE(actual_result == expected_result_hooked);
+    }
 
     const int actual_result = test_func_a(x, y);
-
     REQUIRE(actual_result == expected_result);
 }
